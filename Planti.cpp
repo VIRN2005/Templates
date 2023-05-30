9a290c9c42d15fb4a680d54b42ec9679dc5795a6
Víctor Romero <virn2005@gmail.com>
2023-05-30 15:44:08 -0600

    CPP_6_12211079,,, Actualización

--- Planti.cpp
+++ Planti.cpp
@@ -3,53 +3,8 @@
 #include "Tipos.h"
 #include "MiClase.h"
 
-void aumentar(int& valor, int incremento, Pila <int>& pilas, size_t i) {
-	cout << valor << " ";
-	valor = valor + incremento;
-	pilas.insertar(valor-1);
-}
-void aumentar(double& valor, double incremento, Pila <double>& pilas, size_t i) {
-	cout << valor << " ";
-	valor = valor + incremento;
-	pilas.insertar(valor-1.1);
-}
-
-void aumentar(string& valor, string incremento, Pila<string>& pilas, size_t i) {
-	string Temp = valor + to_string(i);
-	pilas.insertar(Temp);
-	cout << Temp << " ";
-}
-
-void aumentar(MiClase& valor, MiClase incremento, Pila<MiClase>& pila, size_t i) {
-	string Temp = "MC" + to_string(i);
-	MiClase mc(Temp);
-	cout << Temp << " ";
-	pila.insertar(mc);
-}
-
-template <typename tipo>
-void Tipos(tipo valor, tipo incremento, string nombre, const size_t size) {
-	Pila<tipo> pila;
-	cout << "--> Insertar elementos en " << nombre << endl;
-	for (size_t i = 0; i < size; ++i) {
-		aumentar(valor, incremento, pila, i);
-	}
-	cout << "\n<-- Extraer elementos de " << nombre << endl;
-	if (pila.estaVacia()) {
-		cout << "La pila está vacía." << endl;
-	}
-	else {
-		while (!pila.estaVacia()) {
-			cout << pila.encima() << " ";
-			pila.extraer();
-		}
-	}
-	cout << endl;
-}
-
-
 int main() {
-	/*setlocale(LC_ALL, "spanish");*/
+	setlocale(LC_ALL, "spanish");
 
 	Tipos<double>(1.1, 1.1, "doblePila", 5);
 	Tipos<int>(1, 1, "intPila", 10);
--- Tipos.h
+++ Tipos.h
@@ -5,11 +5,42 @@
 #include "MiClase.h"
 using namespace std;
 
-void aumentar(int& valor, int incremento, Pila <int>& pilas, size_t i);
-void aumentar(double& valor, double incremento, Pila <double>& pilad, size_t i);
-void aumentar(string& valor, string incremento, Pila<string>& pilad, size_t i);
-void aumentar(MiClase& valor, MiClase incremento, Pila<MiClase>& pilad, size_t i);
+void aumentar(int& valor, int incremento, Pila <int>& pilas, size_t i) {
+	cout << valor << " ";
+	valor = valor + incremento;
+	pilas.insertar(valor - 1);
+}
+void aumentar(double& valor, double incremento, Pila <double>& pilas, size_t i) {
+	cout << valor << " ";
+	valor = valor + incremento;
+	pilas.insertar(valor - 1.1);
+}
+
+void aumentar(string& valor, string incremento, Pila<string>& pilas, size_t i) {
+	string Temp = valor + to_string(i);
+	pilas.insertar(Temp);
+	cout << Temp << " ";
+}
+
+void aumentar(MiClase& valor, MiClase incremento, Pila<MiClase>& pila, size_t i) {
+	string Temp = "MC" + to_string(i);
+	MiClase mc(Temp);
+	cout << Temp << " ";
+	pila.insertar(mc);
+}
 
 template <typename tipo>
-void Tipos(tipo valor, tipo incremento, string nombre, const size_t size);
+void Tipos(tipo valor, tipo incremento, string nombre, const size_t size) {
+	Pila<tipo> pila;
+	cout << "--> Insertar elementos en " << nombre << endl;
+	for (size_t i = 0; i < size; ++i) {
+		aumentar(valor, incremento, pila, i);
+	}
+	cout << "\n<-- Extraer elementos de " << nombre << endl;
 
+	while (!pila.estaVacia()) {
+		cout << pila.encima() << " ";
+		pila.extraer();
+	}
+	cout << endl;
+}
